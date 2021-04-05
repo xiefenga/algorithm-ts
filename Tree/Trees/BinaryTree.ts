6f8fec74c0c2cd65c25f47f814433899b4783094
xf <1294699027@qq.com>
2021-04-05 19:54:33 +0800

    update implement

--- Tree/Trees/BinaryTree.ts
+++ Tree/Trees/BinaryTree.ts
@@ -1,8 +1,6 @@
 import { TreeNode } from '../TreeNode'
 import { Queue, Stack } from '../../Linear'
 
-const defaultVisit = (val: any) => console.log(val);
-
 abstract class BinaryTree<T> {
 
   protected root: TreeNode<T> | null = null;
@@ -12,6 +10,71 @@ abstract class BinaryTree<T> {
   public abstract remove(val: T): void;
 
   protected abstract createNode(val: T): TreeNode<T>
+
+  public static levelOrderTraversal<T>(tree: BinaryTree<T>): void {
+    const { root } = tree;
+    if (root) {
+      const queue = new Queue<TreeNode<T>>();
+      queue.enqueue(root);
+      while (!queue.isEmpty()) {
+        const node = <TreeNode<T>>queue.dequeue();
+        console.log(node);
+        node.left && queue.enqueue(node.left);
+        node.right && queue.enqueue(node.right);
+      }
+    }
+  }
+
+  public static preorderTraversal<T>(tree: BinaryTree<T>): void {
+    const { root } = tree;
+    if (root) {
+      const stack = new Stack<TreeNode<T>>();
+      stack.push(root);
+      while (!stack.isEmpty()) {
+        const node = <TreeNode<T>>stack.pop();
+        console.log(node);
+        node.right && stack.push(node.right);
+        node.left && stack.push(node.left);
+      }
+    }
+  }
+
+  public static inorderTraversal<T>(tree: BinaryTree<T>): void {
+    const { root } = tree;
+    if (root) {
+      const stack = new Stack<TreeNode<T>>();
+      let node: TreeNode<T> | null = root;
+      while (node || !stack.isEmpty()) {
+        while (node) {
+          stack.push(node);
+          node = node.left;
+        }
+        node = <TreeNode<T>>stack.pop();
+        console.log(node);
+        node = node.right;
+      }
+    }
+  }
+
+  public static postorderTraversal<T>(tree: BinaryTree<T>): void {
+    const { root } = tree;
+    if (root) {
+      const stack = new Stack<TreeNode<T>>();
+      stack.push(root);
+      let prev: TreeNode<T> | null = null; // 前一个弹出/访问的元素
+      while (!stack.isEmpty()) {
+        const top = <TreeNode<T>>stack.peek();
+        if (top.isLeave() || (top.left === prev || top.right === prev)) {
+          prev = <TreeNode<T>>stack.pop();
+          console.log(prev);
+        } else {
+          top.right && stack.push(top.right);
+          top.left && stack.push(top.left);
+        }
+      }
+    }
+  }
+
 
   // 前驱节点，中序遍历时的前一个节点
   protected predecessor(root: TreeNode<T> | null): TreeNode<T> | null {
@@ -52,64 +115,6 @@ abstract class BinaryTree<T> {
     return root.parent;
   }
 
-  public preorderTraversal(visit = defaultVisit, root = this.root): void {
-    if (root) {
-      const stack = new Stack<TreeNode<T>>();
-      stack.push(root);
-      while (!stack.isEmpty()) {
-        const node = stack.pop();
-        visit(node);
-        node.right && stack.push(node.right);
-        node.left && stack.push(node.left);
-      }
-    }
-  }
-
-  public inorderTraversal(visit = defaultVisit, root = this.root): void {
-    if (root) {
-      const stack = new Stack<TreeNode<T>>();
-      let node: TreeNode<T> | null = root;
-      while (node || !stack.isEmpty()) {
-        while (node) {
-          stack.push(node);
-          node = node.left;
-        }
-        node = stack.pop();
-        visit(node);
-        node = node.right;
-      }
-    }
-  }
-
-  public postorderTraversal(visit = defaultVisit, root = this.root): void {
-    if (root) {
-      const stack = new Stack<TreeNode<T>>();
-      stack.push(root);
-      let prev: TreeNode<T> | null = null; // 前一个弹出/访问的元素
-      while (!stack.isEmpty()) {
-        const top = stack.peek();
-        if (top.isLeave() || (top.left === prev || top.right === prev)) {
-          visit(prev = stack.pop());
-        } else {
-          top.right && stack.push(top.right);
-          top.left && stack.push(top.left);
-        }
-      }
-    }
-  }
-
-  public levelOrderTraversal(visit = defaultVisit, root = this.root) {
-    if (root) {
-      const queue = new Queue<TreeNode<T>>();
-      queue.enqueue(root);
-      while (!queue.isEmpty()) {
-        const node = queue.dequeue();
-        visit(node);
-        node.left && queue.enqueue(node.left);
-        node.right && queue.enqueue(node.right);
-      }
-    }
-  }
 }
 
 export default BinaryTree;
